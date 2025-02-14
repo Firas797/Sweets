3c3ba1e36b43432f018847b484a1501722eb05b3
Firas bechedli <fbe.chedli@gmail.com>
2025-02-14 14:33:04 +0100

    lasty3

--- client/src/Componenets/Sweets/SweetsJson.js
+++ client/src/Componenets/Sweets/SweetsJson.js
@@ -1,6 +1,6 @@
 const SweetsJson = [
     {"name": "ڨرانولة", "name2": "Granola", "price": "40", "image": require("../Sweets/SweetsImages/0.jpg")},
-    {"name": "مقروض", "name2": "Makroud", "price": "12", "image": require("../Sweets/SweetsImages/1.webp")},
+    {"name": "مقروض", "name2": "Makroud", "price": "14", "image": require("../Sweets/SweetsImages/1.webp")},
     {"name": "غريبة حمص", "name2": "Ghraybet Homs", "price": "20", "image": require("../Sweets/SweetsImages/2.jpg")},
         { "name": 'Kaak Warka', "name2": 'كعك ورقة ', "price": '50 DT', "image": require("./../../Products Pictures/KAAKWARKA.jpg") },
     
@@ -17,7 +17,7 @@ const SweetsJson = [
     {"name": "Samesa Chocolat", "name2": "Samesa Chocolat", "price": "70", "image": require("../Sweets/SweetsImages/13.jpg")},
     {"name": "صمصمة بوفريوة", "name2": "Samsema Boufrewa", "price": "60", "image": require("../Sweets/SweetsImages/20.jpg")},
     {"name": "صمصمة لوز", "name2": "Samsema Louz", "price": "40", "image": require("../Sweets/SweetsImages/15.jpg")},
-    {"name": "صمصمة جلجلان", "name2": "Samsema Jeljlan", "price": "25", "image": require("../Sweets/SweetsImages/16.jpg")},
+    {"name": "صمصمة جلجلان", "name2": "Samsema Jeljlan", "price": "35", "image": require("../Sweets/SweetsImages/16.jpg")},
     // {"name": "حلو لوز", "name2": "Hlou Louz", "price": "60", "image": require("../Sweets/SweetsImages/17.jpg")}
 ];
 
